0840130504e46fa4df7867702c4baf41964186b7
yanagusti <yana.gusti@gmail.com>
2017-02-24 15:01:11 +0200

    for ubuntu

--- src/Tests/BaseTest.java
+++ src/Tests/BaseTest.java
@@ -19,7 +19,7 @@ public class BaseTest {
 
     @BeforeMethod
     public void SetUp(){
-        System.setProperty("webdriver.chrome.driver", "./drivers/for mac/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
         driver = new ChromeDriver();
         driver.get(baseUrl + "/users/sign_in");
         driver.manage().window().maximize();
